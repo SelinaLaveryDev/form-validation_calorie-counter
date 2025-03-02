76347a457d5b9c1807505b9834e3442e3e02ab8a
Selina Lavery <selina118@hotmail.com>
2025-03-02 17:45:39 +0000

    console logging to see results from the cleanInputString function

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
+  console.log("original string: ", str);
   const regex = /[+-\s]/g; // The g flag stands for global will replace all occurrences of the pattern in the string
   return str.replace(regex, ""); // Replace all occurrences of the pattern with an empty string, basically removing them
 }
+console.log(cleanInputString("+-99"));
