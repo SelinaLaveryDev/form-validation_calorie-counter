8883efb5d1035c3af16a1e9cdb514c3fb77bb020
Selina Lavery <selina118@hotmail.com>
2025-03-05 16:08:58 +0000

    if statement checking if the users input in invalid and setting an alert

--- script.js
+++ script.js
@@ -42,6 +42,9 @@ function getCaloriesFromInputs(list) {
   for (const item of list) {
     const currVal = cleanInputString(item.value);
     const invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+    }
   }
 }
 
